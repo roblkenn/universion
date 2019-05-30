8ae9a427a4007346175fb8ee85d24dc3301901db
Robert Kennedy <robert076kennedy@gmail.com>
2019-05-29 22:35:03 -0400

    chore(pkg-init): Added LICENSE.md and empty python package.

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="UniVersion",
+    version="0.0.1",
+    author="Robert Kennedy",
+    author_email="robert076kennedy@gmail.com",
+    description="An automated versioning tool using semantic versioning and conventional commits that supports a majority of software project types.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/roblkenn/UniVersion",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
